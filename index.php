5b89187be00130d2c884603061083056ff203119
nam <nambolo123vn@gmail.com>
2022-12-22 13:17:04 +0700

    them trang chu

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php include('header.php'); ?>
+<?php include('navbar.php'); ?>
+    <div class="container">
+		<div class="margin-top">
+			<div class="row">
+					<?php include('head.php'); ?>
+					<div class="span12">	
+						<h1>Danh sách sách</h1>
+						
+                            <table cellpadding="0" cellspacing="0" border="0" class="table  table-bordered" id="example">
+                                <thead>
+                                    <tr>
+										<th>No.</th>                                 
+                                        <th>Tên sách</th>                                 
+                                        <th>Thể loại</th>
+										<th>Tác giả</th>
+										<th class="action">Tái bản</th>
+										<th>Nhà xuất bản</th>
+										<th>Nơi xuất bản</th>
+										<th>Mô tả</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+								
+                           
+                                </tbody>
+                            </table>
+							
+			
+			</div>	
+			</div>
+		</div>
+    </div>
+<?php include('footer.php') ?>
