8950a0924add0d596fca6595ed7cb5344b024dd5
nam <nambolo123vn@gmail.com>
2022-12-22 13:20:18 +0700

    them trang thong tin

---
+++ about.php
@@ -0,0 +1,38 @@
+<?php include('header.php'); ?>
+<?php include('navbar.php'); ?>
+    <div class="container">
+		<div class="margin-top">
+			<div class="row">	
+			<?php include('head.php'); ?>
+				
+				<div class="text_content">
+					
+					<table width="450" style="margin: 0pt auto;">
+	
+	<thead><tr><th colspan="3" style="text-align: center;"><h3>Thư viện mở cửa</h3></th></tr></thead>
+	<tbody>
+	<tr>
+	<td colspan="3"></td>
+	</tr>
+	<tr>
+	<td>Thứ hai đến thứ sáu</td>
+	<td>8:00 a.m. đến 7:00 p.m.</td>
+	</tr>
+	<tr>
+	<td>Thứ bảy và chủ nhật</td>
+	<td>8:00 a.m. đến 4:00 p.m.</td>
+	</tr>
+	</tbody>
+	</table>
+	
+
+	
+	</tbody>
+	</table>
+					
+					</div>
+					</div>
+			
+		</div>
+    </div>
+<?php include('footer.php') ?>
