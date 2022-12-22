afe98691fa02afb20a92034537783e92ee03a261
nam <nambolo123vn@gmail.com>
2022-12-22 13:26:32 +0700

    hien sach trong trang chu

--- index.php
+++ index.php
@@ -21,7 +21,34 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-								
+								 
+                                  <?php 
+									$conn = mysqli_connect('localhost','root','','eb_lms');
+								  	$user_query=mysqli_query($conn,"select * from book where status != 'Archive'");
+									while($row=mysqli_fetch_array($user_query)){
+									$id=$row['book_id'];  
+									$cat_id=$row['category_id'];
+									$book_copies = $row['book_copies'];
+									
+									$borrow_details = mysqli_query($conn,"select * from borrowdetails where book_id = '$id' and borrow_status = 'Đang mượn'");
+									$row11 = mysqli_fetch_array($borrow_details);
+									$count = mysqli_num_rows($borrow_details);
+									
+									$total =  $book_copies  -  $count; 
+											$cat_query = mysqli_query($conn,"select * from category where category_id = '$cat_id'");
+											$cat_row = mysqli_fetch_array($cat_query);
+									?>
+									<tr class="del<?php echo $id ?>">
+                                    <td><?php echo $row['book_id']; ?></td>
+                                    <td><?php echo $row['book_title']; ?></td>
+									<td><?php echo $cat_row ['classname']; ?> </td>
+                                    <td><?php echo $row['author']; ?> </td> 
+                                    <td class="action"><?php echo $total;   ?> </td>
+                                     <td><?php echo $row['book_pub']; ?></td>
+									 <td><?php echo $row['publisher_name']; ?></td>
+									 <td><?php echo $row['Describe']; ?></td>
+                                    </tr>
+									<?php  }  ?>
                            
                                 </tbody>
                             </table>
--- navbar.php
+++ navbar.php
@@ -1,5 +1,4 @@
-  		
-	<?php include('tooltip.php'); ?>			
+  					
 	<div class="navbar navbar-fixed-top navbar-inverse">
             <div class="navbar-inner">
                 <div class="container">
@@ -13,11 +12,11 @@
 					<li class="divider-vertical"></li><li class=""><a  rel="tooltip"  data-placement="bottom" title="Home" id="home"   href="index.php"><i class="icon-home icon-large"></i>&nbsp;Trang chủ</a> </li>
 					<li class="divider-vertical"></li>
 					<li class="">
-					<a rel="tooltip"  data-placement="bottom" title="Click Here to About" id="login" href="about.php"><i class="icon-info-sign icon-large"></i>&nbsp;Giới thiệu</a> 
+					<a rel="tooltip"  data-placement="bottom" title="Click Here to About" id="about" href="about.php"><i class="icon-info-sign icon-large"></i>&nbsp;Giới thiệu</a> 
 					</li>
 					<li class="divider-vertical"></li>
 					<li class="">
-					<a rel="tooltip"  data-placement="bottom" title="Click Here to About" id="login" href="librarian/index.php"><i class="icon-user icon-large"></i>&nbsp;Admin</a>
+					<a rel="tooltip"  data-placement="bottom" title="Click Here to About" id="login" href=""><i class="icon-user icon-large"></i>&nbsp;Admin</a>
 					</li>
 				
 					<li class="divider-vertical"></li>
